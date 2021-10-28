58347936403508a908da4afe4a387ad75ff7c084
Or Gur Arie <orgurar@gmail.com>
2021-10-28 04:36:34 +0300

    Almost finished server, before testing

--- server/src/main.c
+++ server/src/main.c
@@ -9,18 +9,23 @@ void term(char *e)
 
 int main(void)
 {
-    // server socket and address
-    int sockfd;
-    struct sockaddr_in servaddr;
-    char buf[BUFSIZE];
-
-    // current connecting client address
-    struct sockaddr_in clientaddr;
-    int slen = sizeof(clientaddr);
-
-    // list of connected client, up to 3
+    // local list of connected client, up to 3
     struct peer clients[MAXCLIENTS];
-    int n = 0;
+    int i, j, n = 0;
+
+    // server socket and address
+    struct sockaddr_in servaddr;
+    int sockfd;
+
+    // initiate servaddr to our local endpoint.
+    // remember that the server should run in a different network
+    // with UDP endpoint previously known and directly accessible
+    // by all clients. The server cannot be behind a NAT!
+    memset((char *)&servaddr, 0, sizeof(servaddr));
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
 
     // create the main server socket
     // AF_INET sets the IPv4 Internet protocols
@@ -28,20 +33,9 @@ int main(void)
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == SOCKERR)
         term("socket creation failed");
 
-    // initiate servaddr to our local endpoint.
-    // remember that the server should run in a different network
-    // with UDP endpoint previously known and directly accessible
-    // by all clients. The server cannot be behind a NAT!
-    memset((char *)&servaddr, 0, sizeof(servaddr));
-
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-
-    // When a socket is created with socket(2), it exists in a name
-    // space(address family) but has no address assigned to it
-    //bind() assigns the address specified by addr to the socket referred to
+    // When a socket is created with socket(), it exists in a name
+    // space(address family) but has no address assigned to it.
+    // bind() assigns the address specified by addr to the socket referred to
     // by the file descriptor sockfd.
     if (bind(sockfd, (struct sockaddr *)(&servaddr), sizeof(servaddr)) == SOCKERR)
         term("bind failed");
@@ -49,12 +43,17 @@ int main(void)
     // start listening to client hole punch datagrams
     while (1)
     {
+        // current connecting client info
+        struct sockaddr_in clientaddr;
+        int slen = sizeof(clientaddr);
+        char buf[BUFSIZE];
+
         // udp message received from a new client
-        if (recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)(&clientaddr), &slen) == SOCKERR)
-            diep("socket recive failed");
         // The client's public UDP endpoint data is now in si_other.
         // the server doesn't care about the datagram's payload,
         // the client's NAT has created the entry
+        if (recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)(&clientaddr), &slen) == SOCKERR)
+            diep("socket recive failed");
 
         printf("%s:%d Just Punched a Hole!\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 
@@ -62,7 +61,29 @@ int main(void)
         clients[n].host = clientaddr.sin_addr.s_addr;
         clients[n].port = clientaddr.sin_port;
         n++;
+
+        // then notify all the connected peers
+        // And then tell everybody about everybody's public UDP endpoints
+        for (i = 0; i < n; i++)
+        {
+            // create a temp client address object
+            clientaddr.sin_addr.s_addr = clients[i].host;
+            clientaddr.sin_port = clients[i].port;
+
+            // send a datagram to each client in our list
+            for (j = 0; j < n; j++)
+            {
+                printf("Sending to %s:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+                // The payload is the client's public UDP endpoint, clients[j]
+                if (sendto(sockfd, &clients[j], 6, 0, (struct sockaddr *)(&clientaddr), slen) == SOCKERR)
+                    diep("socket send failed");
+            }
+        }
+
+        printf("%d peers are currently available\n", n);
     }
 
+    close(sockfd);
     return 0;
 }
